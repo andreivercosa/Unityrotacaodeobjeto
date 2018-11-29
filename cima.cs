5c3092a5c3ea32db3777cdf5c0a59156297d9f26
andreivercosa <andreivercosa@gmail.com>
2018-11-28 21:40:16 -0400

    manipulacao basica

---
+++ cima.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cima : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        //o objeto se moverá para cima, cado o alor seja negativo ele iria para baixo
+        transform.Translate(new Vector3(0,0.1f,0));
+	}
+}
---
+++ lados.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lados : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        //o objeto vai se mover para a direita, caso o valor seja negativo ele se move para a esquerda
+        transform.Translate(new Vector3(0.1f,0,0));
+	}
+}
